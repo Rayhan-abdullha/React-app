72762d394564024858d9ddbb0488be6d7cc4bfb2
Rayhan-abdullha <captainrayhan215@gmail.com>
2021-02-23 16:25:47 +0600

    create react app

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+function App() {
+  var style = {
+    backgroundColor: 'skyblue',
+    color: '#444',
+    padding: '5px'
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Person></Person>
+        <h3 className="" style={style}>Bangla Bhai</h3>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React{" "}
+        </a>{" "}
+      </header>{" "}
+    </div>
+  );
+}
+
+function Person() {
+  return (<div>
+    <h3>React App</h3>
+    <h4>Creat by Abdullah</h4>
+  </div>)
+}
+
+
+export default App;
